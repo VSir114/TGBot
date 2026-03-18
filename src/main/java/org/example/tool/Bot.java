c5dbc1d609cec987f3d6fcf9ac5eeaf2488068fc
VSir <2595931767@qq.com>
2026-03-18 19:01:28 +0800

    添加消息触发事件

--- src/main/java/org/example/tool/Bot.java
+++ src/main/java/org/example/tool/Bot.java
@@ -1,7 +1,11 @@
 package org.example.tool;
 
+import org.example.Message.MessageSend;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
     @Override
@@ -9,18 +13,27 @@ public class Bot extends TelegramLongPollingBot {
         //返回Bot名称
         return "0721Bot";
     }
-
+    //Bot令牌
     @Override
     public String getBotToken() {
-        //Bot令牌
+
         return "";
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         /**
-         * 这是最重要的方法。每当有新的更新可用时，它都会自动调用。
+         * 重要方法。每当有新的更新可用时，它都会自动调用。
          * */
+        Message message = update.getMessage();
+        User from = message.getFrom();
+        String text = message.getText();
+
+        try {
+            MessageSend.SentTextByOne("you send message : "+ text , String.valueOf(from.getId()));
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(update);
     }
 }
