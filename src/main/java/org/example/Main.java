e6890bcfe604d39c58bfe371f1eb93a3bff68037
VSir114 <2595931767@qq.com>
2026-03-18 17:04:41 +0800

    注册Bot实例

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.tool.Bot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi bot = new TelegramBotsApi(DefaultBotSession.class);
+        bot.registerBot(new Bot());
     }
 }
