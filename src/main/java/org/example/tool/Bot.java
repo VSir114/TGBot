3306110277b44e6dd4645b51c9ac222542a061c3
VSir114 <2595931767@qq.com>
2026-03-18 17:05:50 +0800

    添加事件方法

--- src/main/java/org/example/tool/Bot.java
+++ src/main/java/org/example/tool/Bot.java
@@ -18,6 +18,9 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        /**
+         * 这是最重要的方法。每当有新的更新可用时，它都会自动调用。
+         * */
+        System.out.println(update);
     }
 }
