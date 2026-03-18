c49fae1dde85dc865585751997e334cd0e1b1b62
VSir114 <2595931767@qq.com>
2026-03-18 16:56:36 +0800

    继承TG官方接口

---
+++ src/main/java/org/example/tool/Bot.java
@@ -0,0 +1,23 @@
+package org.example.tool;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Bot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        //返回Bot名称
+        return "0721Bot";
+    }
+
+    @Override
+    public String getBotToken() {
+        //Bot令牌
+        return "";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+}
